3c7fa8da56b47286cfc0f584459daa49600b161b
Angelica Herrera <herrerangelik@gmail.com>
2021-08-06 10:30:44 -0500

    nuevos archivos

---
+++ proyectos/50retos/reto1/script.js
@@ -0,0 +1,21 @@
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+    panel.addEventListener("click", () => {
+        removeActiveClasses();
+        panel.classList.add("active");
+    });
+});
+
+function removeActiveClasses() {
+    panels.forEach((panel) => {
+        panel.classList.remove("active");
+    });
+}
+
+// ¿Cuál es la opción que sirve?
+
+//const panels = document.querySelectorAll('.panel')
+//const panels = document.querySelector('.panel')
+//const panels = document.getElementById('panel')
+//const panels = document.getElementsByClassName('.panel')
